1ad1e85c56db04d7a186957325aaf27d0e2aec11
Andrea <andreagiacolono@192.168.1.9>
2024-05-11 10:20:24 +0200

    mosticiattolo azioni gemma e martello

--- Prolog/mostriciattolo/azioni.pl
+++ Prolog/mostriciattolo/azioni.pl
@@ -4,35 +4,37 @@
         - controlliamo che non sia presente del ghiaccio
 */
 /* per applicabile controllo anche che il ghiaccio sia abbattibile o no, quindi se ho il martello è applicabile*/
-applicabile(nord,pos(R,C)) :- 
-    R>1,
-    R1 is R-1,
-    \+ occupata(pos(R1,C)),
-    possiede(martello).
+
+occupata(pos(R,C)) :- 
+    muro(pos(R,C)).
+
+occupata(pos(R,C)) :-
+    gemma(pos(R,C)).
+
+occupata(pos(R,C)) :-
+    \+possiede(martello),
+    ghiaccio(pos(R,C)).
+
 
 applicabile(nord,pos(R,C)) :- 
     R>1,
     R1 is R-1,
-    \+ occupata(pos(R1,C)),
-    \+ ghiaccio(pos(R1,C)). ???
+    \+ occupata(pos(R1,C)).
 
 applicabile(sud,pos(R,C)) :- 
     num_righe(NR), R<NR,
     R1 is R+1,
-    \+ occupata(pos(R1,C)),
-    \+ ghiaccio(pos(R1,C)).
+    \+ occupata(pos(R1,C)).
 
 applicabile(ovest,pos(R,C)) :- 
     C>1,
     C1 is C-1,
-    \+ occupata(pos(R,C1)),
-    \+ ghiaccio(pos(R,C1)).
+    \+ occupata(pos(R,C1)).
 
 applicabile(est,pos(R,C)) :- 
     num_col(NC), C<NC,
     C1 is C+1,
-    \+ occupata(pos(R,C1)),
-    \+ ghiaccio(pos(R,C1)).
+    \+ occupata(pos(R,C1)).
 
 trasforma(est,pos(R,C),pos(R,C1)) :- 
     applicabile(est, pos(R,C+1)),
@@ -58,8 +60,3 @@ raccogli(pos(R,C)) :-
     martello(pos(R,C)),
     \+ possiede(martello),
     assert(possiede(martello)).
-
-raccogli(pos(R,C)) :-
-    gemma(pos(R,C)),
-    \+ raccolte(pos(R,C)),
-    assert(raccolte(pos(R,C))).
