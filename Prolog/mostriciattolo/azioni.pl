ad276f19f509516115bff6f44dc620cc575c4280
Andrea <andreagiacolono@192.168.1.9>
2024-05-11 10:41:28 +0200

    mosticiattolo azioni gemma e martello

--- Prolog/mostriciattolo/azioni.pl
+++ Prolog/mostriciattolo/azioni.pl
@@ -60,3 +60,34 @@ raccogli(pos(R,C)) :-
     martello(pos(R,C)),
     \+ possiede(martello),
     assert(possiede(martello)).
+
+
+muovi_gemme :-
+    findall(pos(R,C), gemma(pos(R,C)), Gemme), # Trova tuttel el pos delle gemme
+    muovi_lista_gemme(Gemme).
+
+muovi_lista_gemme([]). #Se non ci sono più gemme da muovere, termina
+muovi_lista_gemme(nord, [pos(R,C)|Tail]) :-
+    trasforma(nord,pos(R,C), pos(R1,C))
+    retract(gemma(pos(R,C))), #Possiamo usare il retract?? Altrimenti dobbiamo tenere traccia della lista di posizioni delle gemme e passarla come parametro a ogni chiamata
+    assert(gemma(pos(R1,C))),
+    muovi_lista_gemme(Rest).
+
+#Verifica per il bonus
+tre_gemme_adiacenti([G1, G2, G3]) :-
+    adiacenti(G1, G2),  % Controlla se G1 è adiacente a G2
+    adiacenti(G2, G3),  % Controlla se G2 è adiacente a G3
+    adiacenti(G1, G3).  % Controlla se G1 è adiacente a G3
+
+adiacenti(pos(R1,C1), pos(R2,C2)) :-
+    D_R is abs(R1 - R2),
+    D_C is abs(C1 - C2),
+    D_R =:= 1,
+    D_C =:= 0.
+
+adiacenti(pos(R1,C1), pos(R2,C2)) :-
+    D_R is abs(R1 - R2),
+    D_C is abs(C1 - C2),
+    D_R =:= 0,
+    D_C =:= 1.
+    # non so se possiamo usare l'or  (   (D_R =:= 1, D_C =:= 0) ; (D_R =:= 0, D_C =:= 1) ).
