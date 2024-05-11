64d7385fb6858bb732ba44a0a7803dcd154577d8
Andrea <andreagiacolono@192.168.1.9>
2024-05-11 09:36:17 +0200

    mosticiattolo azioni gemma e martello

--- Prolog/mostriciattolo/azioni.pl
+++ Prolog/mostriciattolo/azioni.pl
@@ -3,12 +3,18 @@
         - controlliamo che non sia occupata da un muro fisso
         - controlliamo che non sia presente del ghiaccio
 */
+/* per applicabile controllo anche che il ghiaccio sia abbattibile o no, quindi se ho il martello è applicabile*/
+applicabile(nord,pos(R,C)) :- 
+    R>1,
+    R1 is R-1,
+    \+ occupata(pos(R1,C)),
+    possiede(martello).
 
 applicabile(nord,pos(R,C)) :- 
     R>1,
     R1 is R-1,
     \+ occupata(pos(R1,C)),
-    \+ ghiaccio(pos(R1,C)).
+    \+ ghiaccio(pos(R1,C)). ???
 
 applicabile(sud,pos(R,C)) :- 
     num_righe(NR), R<NR,
@@ -47,3 +53,13 @@ trasforma(nord,pos(R,C),pos(R1,C)) :-
     applicabile(nord, pos(R1,C)),
     R1 is R-1,
     trasforma(nord,pos(R,C), pos(R1,C)).
+
+raccogli(pos(R,C)) :-
+    martello(pos(R,C)),
+    \+ possiede(martello),
+    assert(possiede(martello)).
+
+raccogli(pos(R,C)) :-
+    gemma(pos(R,C)),
+    \+ raccolte(pos(R,C)),
+    assert(raccolte(pos(R,C))).
