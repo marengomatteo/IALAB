82c3faef90690d4e0f7be74a46d006eed0f34839
Andrea <andreagiacolono@192.168.1.9>
2024-05-11 10:20:43 +0200

    mosticiattolo azioni gemma e martello

--- Prolog/mostriciattolo/mostriciattolo.pl
+++ Prolog/mostriciattolo/mostriciattolo.pl
@@ -4,16 +4,16 @@ num_col(8).
 iniziale(pos(1,5)).
 finale(pos(4,8)).
 
-occupata(pos(1,6)).
-occupata(pos(2,8)).
-occupata(pos(3,8)).
-occupata(pos(4,4)).
-occupata(pos(4,5)).
-occupata(pos(5,5)).
-occupata(pos(6,2)).
-occupata(pos(7,2)).
-occupata(pos(7,6)).
-occupata(pos(8,3)).
+muro(pos(1,6)).
+muro(pos(2,8)).
+muro(pos(3,8)).
+muro(pos(4,4)).
+muro(pos(4,5)).
+muro(pos(5,5)).
+muro(pos(6,2)).
+muro(pos(7,2)).
+muro(pos(7,6)).
+muro(pos(8,3)).
 
 ghiaccio(pos(2,6)).
 ghiaccio(pos(2,7)).
