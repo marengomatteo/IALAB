bd040dbbe7dfc50cd35218c29d65ade32b6d9ee9
Matteo  Marengo <matteo.marengo@excentio.com>
2024-05-13 22:56:41 +0200

    prove varie su trasforma: non funziona ancora

--- Prolog/mostriciattolo/azioni.pl
+++ Prolog/mostriciattolo/azioni.pl
@@ -7,8 +7,8 @@
 
 occupata(pos(R,C)) :- 
     muro(pos(R,C));
-    gemma(pos(R,C));
-    (\+possiede(martello), ghiaccio(pos(R,C))).
+    gemma(pos(R,C)).
+    /*(\+possiede(martello), ghiaccio(pos(R,C))).*/
 
 
 applicabile(nord,pos(R,C)) :- 
@@ -16,89 +16,91 @@ applicabile(nord,pos(R,C)) :-
     R1 is R-1,
     \+ occupata(pos(R1,C)).
 
-applicabile(sud,pos(R,C)) :- 
-    num_righe(NR), R<NR,
-    R1 is R+1,
-    \+ occupata(pos(R1,C)).
-
 applicabile(ovest,pos(R,C)) :- 
     C>1,
     C1 is C-1,
     \+ occupata(pos(R,C1)).
+
+applicabile(sud,pos(R,C)) :- 
+    num_righe(NR), R<NR,
+    R1 is R+1,
+    \+ occupata(pos(R1,C)).
 
 applicabile(est,pos(R,C)) :- 
     num_col(NC), C<NC,
     C1 is C+1,
     \+ occupata(pos(R,C1)).
 
+
+nonRipetere(_,[]).
+nonRipetere(Az, [LastAz|_]) :- Az \= LastAz.
+
+
 /* --- TRASFORMA EST --- */
 trasforma(est,pos(R,C),pos(R,C1)) :- 
-    applicabile(est, pos(R,C+1)),
-    C1 is C+1,
-    raccogli(pos(R,C1)),
-    trasforma(est,pos(R,C), pos(R,C1)),!.
-
-trasforma(est,pos(R,C),pos(R,C1)) :- 
-    applicabile(est, pos(R,C+1)),
+    applicabile(est,pos(R,C)),
     C2 is C+1,
-    trasforma(est,pos(R,C2), pos(R,C1)),!.
+    raccogli(pos(R,C2)),
+    trasforma(est,pos(R,C2), pos(R,C1)).
 
 trasforma(est,pos(R,C),pos(R,C1)) :- 
-  (C < 8 ->  C1 is C + 1 ;   C =:= 8, C1 is C),!.
+    applicabile(est,pos(R,C)),
+    C2 is C+1,
+    trasforma(est,pos(R,C2), pos(R,C1)).
+
+trasforma(est,pos(R,C),pos(R,C)).
 
 /* --- TRASFORMA OVEST --- */
 
-trasforma(ovest,pos(R,C),pos(R,C1)) :- 
-    applicabile(ovest, pos(R,C-1)),
+trasforma(ovest,pos(R,C), _ , pos(R,C1)) :-
+    applicabile(ovest,pos(R,C)),
     C2 is C-1,
     raccogli(pos(R,C2)),
-    trasforma(ovest,pos(R,C2), pos(R,C1)),!.
+    trasforma(ovest,pos(R,C2),ovest, pos(R,C1)).
 
-trasforma(ovest,pos(R,C),pos(R,C1)) :- 
-    applicabile(ovest, pos(R,C-1)),
+trasforma(ovest,pos(R,C), _ ,pos(R,C1) ) :- 
+    applicabile(ovest, pos(R,C)),
     C2 is C-1,
-    trasforma(ovest,pos(R,C2), pos(R,C1)),!.
+    trasforma(ovest,pos(R,C2), ovest , pos(R,C1)).
 
-trasforma(ovest, pos(R,C), pos(R,C1)) :-
-    (C > 1 ->  C1 is C - 1 ;   C =:= 1, C1 is C),!.
+trasforma(ovest,pos(R,C), LastAz , pos(R,C)) :- LastAz =:= ovest. 
 
 /* --- TRASFORMA SUD --- */
 
 trasforma(sud,pos(R,C),pos(R1,C)) :- 
-    applicabile(sud, pos(R+1,C)),
-    R1 is R+1,
-    trasforma(sud,pos(R,C), pos(R1,C)),!.
-
-trasforma(sud,pos(R,C),pos(R1,C)) :- 
-    applicabile(sud, pos(R+1,C)),
+    applicabile(sud, pos(R,C)),
     R2 is R+1,
     raccogli(pos(R2,C)),
-    trasforma(sud,pos(R2,C), pos(R1,C)),!.
+    trasforma(sud,pos(R2,C), pos(R1,C)).
 
 trasforma(sud,pos(R,C),pos(R1,C)) :- 
-    (R < 8 ->  R1 is R + 1 ;   R =:= 8, R1 is R),!.
+    applicabile(sud, pos(R,C)),
+    R2 is R+1,
+    trasforma(sud,pos(R2,C), pos(R1,C)).
+
+trasforma(sud,pos(R,C),pos(R,C)). 
 
 /* --- TRASFORMA NORD --- */
 
 trasforma(nord,pos(R,C),pos(R1,C)) :- 
-    applicabile(nord, pos(R-1,C)),
+    applicabile(nord,pos(R,C)),
     R2 is R-1,
     raccogli(pos(R2,C)),
-    trasforma(nord,pos(R2,C), pos(R1,C)),!.
+    trasforma(nord,pos(R2,C), pos(R1,C)).
 
 trasforma(nord,pos(R,C),pos(R1,C)) :- 
-    applicabile(nord, pos(R-1,C)),
+    applicabile(nord,pos(R,C)),
     R2 is R-1,
-    trasforma(nord,pos(R2,C), pos(R1,C)),!.
+    trasforma(nord,pos(R2,C), pos(R1,C)).
 
-trasforma(nord,pos(R,C),pos(R1,C)) :- 
-  (R > 1 ->  R1 is R - 1 ;   R =:= 1, R1 is R),!.
+trasforma(nord,pos(R,C),pos(R,C)).
 
+% raccogli(pos(R,C)) :-
+%     martello(pos(R,C)),
+%     \+ possiede(martello),
+%     assert(possiede(martello)).
 
-raccogli(pos(R,C)) :-
-    martello(pos(R,C)),
-    \+ possiede(martello),
-    assert(possiede(martello)).
+raccogli(_) :- 1>1.
 
 
 muovi_gemme(Dir) :-
---
+++ Prolog/mostriciattolo/iterative-deepening.pl
@@ -0,0 +1,26 @@
+/* ricerca */
+ricerca(Cammino,Profondita,Step,Soglia):-
+    iniziale(S0),
+    itdeep(S0,Profondita,Soglia,Step,Cammino).
+
+/* iterative deepening */
+itdeep(S,Profondita,Soglia,_,Cammino) :-
+    Profondita =< Soglia,
+    ric_prof(S,Profondita,[],Cammino).
+
+itdeep(S,Profondita,Soglia,Step,Cammino) :-
+    NuovaProf is Profondita + Step,
+    itdeep(S,NuovaProf,Soglia,Step,Cammino).
+
+/* ricerca in profondità limitata */
+
+ric_prof(S,_,_,[]) :- finale(S),!.
+
+ric_prof(S,Profondita,Visitati,[Az|SeqAzioni]):-
+    Profondita > 0,
+    nonRipetere(Az,SeqAzioni),
+    trasforma(Az,S, _ , SNuovo),
+    NuovaProfondita is Profondita-1,
+    ric_prof(SNuovo,NuovaProfondita,[S|Visitati],SeqAzioni).
+
+
