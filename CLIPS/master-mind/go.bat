ca90d211de1f02a450b80e0e8278b86ed67025ad
Marco Frattarola <marco.fratta97@gmail.com>
2024-06-22 19:10:50 +0200

    Abbozzo intelligente con condizioni

--- CLIPS/master-mind/go.bat
+++ CLIPS/master-mind/go.bat
@@ -1,6 +1,7 @@
-
-(load 0_Main.clp)
-(load 1_Game.clp)
-(load 2_Code.clp)
-(load 3_Agent.clp)
+(clear)
+(load C:/Users/marco/OneDrive/Desktop/IALAB/IALAB-1/CLIPS/master-mind/0_Main.clp)
+(load C:/Users/marco/OneDrive/Desktop/IALAB/IALAB-1/CLIPS/master-mind/1_Game.clp)
+(load C:/Users/marco/OneDrive/Desktop/IALAB/IALAB-1/CLIPS/master-mind/2_Code.clp)
+(load C:/Users/marco/OneDrive/Desktop/IALAB/IALAB-1/CLIPS/master-mind/3_Agent.clp)
+(load C:/Users/marco/OneDrive/Desktop/IALAB/IALAB-1/CLIPS/master-mind/6_test_permutation.clp)
 (reset)
